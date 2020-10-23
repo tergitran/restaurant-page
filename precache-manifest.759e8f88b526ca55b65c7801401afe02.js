1cd468822debb73271a2a9a12d99f3ed635af529
tergitran <tergitran@gmail.com>
2020-10-23 17:00:56 +0700

    Updates

---
+++ precache-manifest.759e8f88b526ca55b65c7801401afe02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527004a8d7fb6f69d3d41e2af1291123",
+    "url": "/restaurant-page/index.html"
+  },
+  {
+    "revision": "d1a4c5bf896017fcff17",
+    "url": "/restaurant-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7b075acc538c9ac79b7",
+    "url": "/restaurant-page/static/js/2.61e8053b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/restaurant-page/static/js/2.61e8053b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a4c5bf896017fcff17",
+    "url": "/restaurant-page/static/js/main.a036670d.chunk.js"
+  },
+  {
+    "revision": "e06f1df0742c207c61ed",
+    "url": "/restaurant-page/static/js/runtime-main.7b9989d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restaurant-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
